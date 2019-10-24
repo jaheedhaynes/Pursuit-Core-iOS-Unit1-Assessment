20f9bcb4005bc291109ba10ea882baf07559e938
Jaheed Haynes <jaheedhaynes@Jaheeds-MacBook-Pro.local>
2019-10-24 11:53:45 -0400

    update

---
+++ BlackJack/BlackJack/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  BlackJack
+//
+//  Created by Jaheed Haynes on 10/24/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+let deck = [Card]()
+let player = Player.playerName
+let hitPlayer = Bool()
+
